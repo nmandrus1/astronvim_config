3d5e32a7738a8439955d2ff3c3d7d020f0ca0794
nmandrus1 <nmandrus1@gmail.com>
2022-12-21 17:00:19 -0500

    changed guifont

--- init.lua
+++ init.lua
@@ -47,6 +47,7 @@ local config = {
       spell = false, -- sets vim.opt.spell
       signcolumn = "auto", -- sets vim.opt.signcolumn to auto
       wrap = false, -- sets vim.opt.wrap
+      guifont = { "JetBrainsMono_Nerd_Font:h10" }
     },
     g = {
       mapleader = " ", -- sets vim.g.mapleader
